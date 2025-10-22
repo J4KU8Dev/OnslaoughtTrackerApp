9f3425c1582ed060f423a478e2aa812ca692c2bf
Jakub <kubarafalski240703@gmail.com>
2025-10-22 19:14:21 +0200

    update 22.10

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Header } from "./header/header";
+import { User } from "./ComponentsUser/user/user";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header, User],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('onslaugthTrackerApp');
+  users:string[] = ["Nie_Po_Sionym","Guuby_come_b4ck_ag4in","Markson67","betoon3"];
 }
